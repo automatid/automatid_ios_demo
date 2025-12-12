4eb88de44ac1f1b94753d5227d5a0308311e6abc
sem.dangelo <s.dangelo@opentech.com>
2025-12-12 14:38:17 +0100

    Rilascio versione 4.0.3

---
+++ AutomatID/SceneDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  SceneDelegate.swift
+//  SwissBankers
+//
+//  Created by Silvio D'Angelo on 12/11/25.
+//  Copyright © 2025 Opentech. All rights reserved.
+//
+
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+    
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        // Confirm the scene is a window scene in iOS or iPadOS.
+        guard let windowScene = scene as? UIWindowScene else { return }
+
+        let win: UIWindow = UIWindow(windowScene: windowScene)
+        window = win
+    }
+}
